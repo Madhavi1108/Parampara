17e970b438ae024cfc7368f7a6c707d141bca9c7
om-dev007 <officialomnarayan@gmail.com>
2026-07-07 15:06:47 +0530

    refactor(artisans): return explicit 404 response for missing artisan

--- controllers/artisan.controller.js
+++ controllers/artisan.controller.js
@@ -3,11 +3,8 @@ const store = require('../data/store');
 // Get all artisans
 exports.getAllArtisans = (req, res, next) => {
   try {
-    const artisans = Array.isArray(store.artisans) ? [...store.artisans] : [];
-
-    res.json({
-      data: artisans,
-    });
+    const artisans = store.artisans || [];
+    res.json(artisans);
   } catch (err) {
     next(err);
   }
@@ -18,11 +15,11 @@ exports.getArtisanById = (req, res, next) => {
   try {
     const { id } = req.params;
     const artisan = (store.artisans || []).find((a) => a.id === id);
+
     if (!artisan) {
-      const error = new Error('Artisan not found');
-      error.status = 404;
-      throw error;
+      return res.status(404).json({ error: 'Artisan not found' });
     }
+
     res.json(artisan);
   } catch (err) {
     next(err);
